eeae34962af75c93312130f09f21434fafa20193
bharath2468 <bharath29704@gmail.com>
2024-07-01 22:51:12 +0530

    Done

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
+"""with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()"""
 
 
 __version__ = "0.0.0"
