2d186eaeb5679ff549ad48956479628a5f333c71
bharath2468 <bharath29704@gmail.com>
2024-07-02 11:28:21 +0530

    Finished

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
-"""with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()"""
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
 
  
 __version__ = "0.0.0"
@@ -18,7 +18,7 @@ setuptools.setup(
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="A small python package for skin disease classification",
-    long_description="Skin-Disease-Classification",
+    long_description=long_description,
     long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
